6b9f249edf3642a4ddf6c04f21531a25edc0cb4a
sakakibara <sakakibara@example.com>
2022-10-06 06:48:15 +0900

    クラス名をアンダーバーに変更

--- components/Home.tsx
+++ components/Home.tsx
@@ -8,11 +8,11 @@ export default function Home(): JSX.Element {
       <header className="text-center font-lusi text-[32px] leading-[48px] text-accent">My Web Sample</header>
       <div className="h-[48px] bg-main"></div>
       <form action="" className="mx-auto mt-[32px] w-2/3 max-w-xs">
-        <label htmlFor="name" className={styles['label-yellow']}>
+        <label htmlFor="name" className={styles.label_yellow}>
           ユーザー名
         </label>
         <input type="text" name="name" className="mb-[24px] block w-full border-[1px] " />
-        <label htmlFor="password" className={styles['label-yellow']}>
+        <label htmlFor="password" className={styles.label_yellow}>
           パスワード
         </label>
         <input type="text" name="password" className="mb-[24px] block w-full border-[1px]" />

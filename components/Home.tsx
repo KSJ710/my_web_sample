4fbdebb896d0b4f19f2db5b1182b1125af8168bf
sakakibara <sakakibara@example.com>
2022-10-05 21:37:16 +0900

    ログインページのスタイルを変更

--- components/Home.tsx
+++ components/Home.tsx
@@ -5,9 +5,9 @@ import styles from './Home.module.scss'
 export default function Home(): JSX.Element {
   return (
     <div>
-      <header className="text-center font-lusi text-[24px] leading-[48px] text-accent">My Web Sample</header>
+      <header className="text-center font-lusi text-[32px] leading-[48px] text-accent">My Web Sample</header>
       <div className="h-[48px] bg-main"></div>
-      <form action="" className="mx-auto mt-[32px] w-2/3">
+      <form action="" className="mx-auto mt-[32px] w-2/3 max-w-xs">
         <label htmlFor="name" className={styles['label-yellow']}>
           ユーザー名
         </label>

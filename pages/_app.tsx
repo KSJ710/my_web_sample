8e2851f479833322055d42c314bb1c8cc8e72636
sakakibara <sakakibara@example.com>
2022-10-09 21:27:41 +0900

    next-auth設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '../styles/globals.scss'
-import Base from 'components/layout/Base'
 import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
+import { SessionProvider } from 'next-auth/react'
+import Base from 'components/layout/Base'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   if (Component?.props?.authStatus === 'authenticated') {
     return (
-      <Base>
-        <Component {...pageProps} />
-      </Base>
+      <SessionProvider session={session}>
+        <Base>
+          <Component {...pageProps} />
+        </Base>
+      </SessionProvider>
     )
   } else if (Component?.props?.authStatus === 'noAuthenticated') {
     return (

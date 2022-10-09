a186e1ba9652cf14fd97a26d3cb6d5ae6d30f26a
sakakibara <sakakibara@example.com>
2022-10-09 21:27:11 +0900

    不要コード削除

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,15 +1,15 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from './Login.module.scss'
 
 export default function Login(): JSX.Element {
   return (
     <form action="" className="mx-auto mt-[32px] w-2/3 max-w-[320px]">
-      <label htmlFor="name" className={styles.label_yellow}>
+      <label htmlFor="name" className="label_yellow">
         ユーザー名
       </label>
       <input type="text" name="name" className="mb-[24px] block w-full border-[1px] " />
-      <label htmlFor="password" className={styles.label_yellow}>
+      <label htmlFor="password" className="label_yellow">
         パスワード
       </label>
       <input type="text" name="password" className="mb-[24px] block w-full border-[1px]" />

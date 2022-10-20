6157aa9ec35abc9022296a9dfa5c4f6347a2936b
sakakibara <sakakibara@example.com>
2022-10-20 11:53:51 +0900

    スキーマ変更

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ import role from './seed_data/role'
 import members from './seed_data/members'
 import posts from './seed_data/posts'
 import tags from './seed_data/tags'
+import tagCategories from './seed_data/tag_categories'
 import users from './seed_data/users'
 
 const prisma = new PrismaClient()
@@ -10,6 +11,7 @@ const prisma = new PrismaClient()
 async function main() {
   await prisma.mRole.createMany({ data: role })
   await prisma.member.createMany({ data: members })
+  await prisma.mTagCategory.createMany({ data: tagCategories })
   await prisma.mTag.createMany({ data: tags })
   await prisma.post.createMany({ data: posts })
   await prisma.user.createMany({ data: users })

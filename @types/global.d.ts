c1297a8f9dc23ab5e682b60a5f9512ad46516c56
sakakibara <sakakibara@example.com>
2022-10-20 11:54:15 +0900

    型定義追加

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -10,3 +10,14 @@ type Post = {
   createdAt: Date
   updatedAt: Date
 }
+
+type PostJson = {
+  id: number
+  title: string
+  linkPath: string | null
+  imagePath: string | null
+  published: boolean
+  authorId: number
+  createdAt: string
+  updatedAt: string
+}

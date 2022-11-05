74fc221b399e54935aefd777537214f38f79dacb
KSJ710 <KSJ710@example.com>
2022-11-05 11:09:42 +0900

    dummy画像をローカルから表示

--- helpers/common.ts
+++ helpers/common.ts
@@ -15,8 +15,3 @@ export function stringDateToYmd(stringDate: string) {
   let dd = ('00' + date.getDate()).slice(-2)
   return yyyy + ' / ' + MM + ' / ' + dd
 }
-
-// ダミー画像読み込み
-export const dummyImage = () => {
-  return `https://prtimes.jp/i/2663/1169/resize/d2663-1169-349d5248bc5915b3c06c-0.jpg`
-}

62b60eead7fe57172abea6cfaae7577f7c93c68c
KSJ710 <KSJ710@example.com>
2022-11-17 23:55:40 +0900

    モデル型を追加

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,12 +1,133 @@
-type User = {
-  id: string
-  roleId: number
+/**
+ * Model MRole
+ *
+ */
+type MRole = {
+  id: number
   name: string
-  email: string
   createdAt: Date
   updatedAt: Date
 }
 
+/**
+ * Model MTag
+ *
+ */
+type MTag = {
+  id: number
+  name: string
+  categoryId: number
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model MTagCategory
+ *
+ */
+type MTagCategory = {
+  id: number
+  name: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model Member
+ *
+ */
+type Member = {
+  id: number
+  name: string
+  email: string
+  password: string
+  rememberToken: string | null
+  rememberExpiresAt: Date | null
+  confirmToken: string | null
+  confirmExpiresAt: Date | null
+  confirmStatus: string
+  image: string | null
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model Account
+ *
+ */
+type Account = {
+  id: string
+  userId: string
+  type: string
+  provider: string
+  providerAccountId: string
+  refresh_token: string | null
+  access_token: string | null
+  expires_at: number | null
+  token_type: string | null
+  scope: string | null
+  id_token: string | null
+  session_state: string | null
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model Session
+ *
+ */
+type Session = {
+  id: string
+  sessionToken: string
+  userId: string
+  expires: Date
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model User
+ *
+ */
+type User = {
+  id: string
+  roleId: number
+  name: string | null
+  email: string | null
+  emailVerified: Date | null
+  image: string | null
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model VerificationToken
+ *
+ */
+type VerificationToken = {
+  identifier: string
+  token: string
+  expires: Date
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model PostBelongToTag
+ *
+ */
+type PostBelongToTag = {
+  id: number
+  postId: number
+  tagId: number
+  createdAt: Date
+  updatedAt: Date
+}
+
+/**
+ * Model Post
+ *
+ */
 type Post = {
   id: number
   title: string
@@ -18,9 +139,14 @@ type Post = {
   updatedAt: Date
 }
 
-type Category = {
+/**
+ * Model Profile
+ *
+ */
+type Profile = {
   id: number
-  name: string
+  bio: string | null
+  userId: string
   createdAt: Date
   updatedAt: Date
 }

87e2f1c75de38d72aa68dda4b5b886488271faf5
KSJ710 <KSJ710@example.com>
2022-11-23 10:49:27 +0900

    型追加

--- app/auth/adm/Adm.tsx
+++ app/auth/adm/Adm.tsx
@@ -1,3 +1,8 @@
+type Propse = {
+  categories: Promise<JSX.Element>
+  posts: Promise<JSX.Element>
+}
+
 export default function Adm(props: any): JSX.Element {
   // const [selectedTag, setSelectedTag] = useState(null)
 

d42c378b0b4ff59fb4dc2e3eff657f586479cb68
KSJ710 <KSJ710@example.com>
2022-12-03 13:15:59 +0900

    パス変更

--- app/adm/page.tsx
+++ app/adm/page.tsx
@@ -3,16 +3,18 @@ import Adm from './Adm'
 import Categories from './Categories'
 import Posts from './Posts'
 
-export default function Page() {
+export default function Page(): JSX.Element {
   return (
     <Adm
       categories={
         <Suspense fallback={<p>Loading...1</p>}>
+          {/* @ts-expect-error Server Component */}
           <Categories />
         </Suspense>
       }
       posts={
         <Suspense fallback={<p>Loading...2</p>}>
+          {/* @ts-expect-error Server Component */}
           <Posts />
         </Suspense>
       }

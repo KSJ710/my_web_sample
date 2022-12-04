ae7f1e19df947bb35fbfd3f48ffe1db6c31ffefd
KSJ710 <KSJ710@example.com>
2022-12-05 03:36:18 +0900

    単体テスト

--- __tests__/app/adm/page.test.tsx
+++ __tests__/app/adm/page.test.tsx
@@ -1,52 +1,18 @@
-import { render, screen } from '@testing-library/react'
-import { prisma } from 'lib/prisma'
-import { PrismaClient } from '@prisma/client'
-import Page from 'app/auth/adm/page'
+// 非同期コンポーネントのレンダリング出来ず
+import { replaceOverWordCount } from 'app/adm/Category'
 import { act } from 'react-dom/test-utils'
-import { unmountComponentAtNode } from 'react-dom'
 
-let container: any = null
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div')
-  document.body.appendChild(container)
-})
+beforeEach(() => {})
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container)
-  container.remove()
-  container = null
-})
+afterEach(() => {})
 
-describe('auth/adm', () => {
-  it('render page unchanged', async () => {
-    const date = new Date('2000-1-1')
-    const category: MTagCategory[] = [
-      {
-        id: 1,
-        name: 'mock',
-        createdAt: date,
-        updatedAt: date
-      }
-    ]
-
-    const post: Post[] = [
-      {
-        id: 1,
-        title: 'mock',
-        linkPath: 'http://',
-        imagePath: null,
-        published: true,
-        authorId: '1',
-        createdAt: date,
-        updatedAt: date
-      }
-    ]
-
-    await act(async () => {
-      render(<Page />, container)
-    })
-    expect(container).toMatchSnapshot()
+describe('auth/adm replaceOverWordCount', () => {
+  it('fn short word', () => {
+    const result = replaceOverWordCount('123456789012345')
+    expect(result).toBe('123456789012345')
+  })
+  it('fn long word', () => {
+    const result = replaceOverWordCount('1234567890123456')
+    expect(result).toBe('123456789012345...')
   })
 })
